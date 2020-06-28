73ae716d34a123d20db4b8a58f822898d5c91079
Jack Vomacka <vomacka@usc.edu>
2020-06-28 14:31:49 -0700

    Tested dealer functionality

--- backend/sam-app/hello_world/app.py
+++ backend/sam-app/hello_world/app.py
@@ -1,14 +1,18 @@
 import json
+import random
 
 suits = ["H", "D", "S", "C"]
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 class Dealer:
-    numDecks = 0
-    decks = {}      # (deck ID, deck[])
-    players = {}    # (deck ID, players[])
 
-    def singleDeck():
+    def __init__(self):
+        self.numDecks = 0
+        self.decks = {}      # (deck ID, deck[])
+        self.players = {}    # (deck ID, players[])
+        self.hands = {}      # (deck ID, (player ID, hand[]))
+
+    def singleDeck(self):
         newDeck = []
         for s in suits:
             for r in ranks:
@@ -16,25 +20,27 @@ class Dealer:
 
         random.shuffle(newDeck) 
 
-        decks[numDecks] = newDeck                           # numDeck serves as the ID of newDeck
-        numDecks += 1
+        self.decks[self.numDecks] = newDeck                           # numDeck serves as the ID of newDeck
+        self.players[self.numDecks] = []
+        self.hands[self.numDecks] = {}
+        self.numDecks += 1
 
-        return (numDecks - 1)
+        return (self.numDecks - 1)
 
-    def addPlayer(deckID, playerName):
-        decks[deckID].append(playerName)
-        return  len(decks[deckID])                          # player's ID is deck-specific (its position in the deck's player array)
+    def addPlayer(self, deckID, playerName):
+        playerID = len(self.players[deckID])
+        self.players[deckID].append(playerName)
+        self.hands[deckID][playerID] = []
+        return  (len(self.players[deckID]) - 1)                          # player's ID is deck-specific (its position in the deck's player array)
 
-    def draw(deckID, playerID):
-        index = random.randrange(0, len(decks[deckID]))
-        card = decks[deckID].pop(index)                     # (rank, suit)
-        players[deckID][playerID].append(card)
+    def draw(self, deckID, playerID):
+        index = random.randrange(0, len(self.decks[deckID]))
+        card = self.decks[deckID].pop(index)                     # (rank, suit)
+        self.hands[deckID][playerID].append(card)
         return card
 
-    def showHands(deckID):
-        return decks[deckID]
-
-
+    def showHands(self, deckID):
+        return self.hands[deckID]
 
 
 def lambda_handler(event, context):
